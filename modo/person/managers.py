cd41f058b0a34cdbbead8a19ccb7f87214bd0716
walter_wu090 <walter.wu090@gmail.com>
2018-04-20 09:27:25 -0400

    Add manager for model

---
+++ modo/person/managers.py
@@ -0,0 +1,30 @@
+from django.contrib.auth.base_user import BaseUserManager
+
+
+class HumanManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **kwargs):
+
+        if not email:
+            raise ValueError('Email is required')
+
+        email = self.normalize_email(email)
+        human = self.model(email=email, **kwargs)
+        human.set_password(password)
+        human.save(using=self._db)
+
+        return human
+
+    def create_user(self, email, password, **kwargs):
+        kwargs.setdefault('is_superuser', False)
+        return self.create_user(email, password, **kwargs)
+
+    def create_superuser(self, email, password, **kwargs):
+        kwargs.setdefault('is_superuser', True)
+        kwargs.setdefault('is_staff', True)
+
+        if kwargs.get('is_superuser') is not True:
+            raise ValueError('The user is not a superuser')
+
+        return self._create_user(email, password, **kwargs)
