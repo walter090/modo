decd867fbbe61b4a6d9c862e7937f7923e336060
walter_wu090 <walter.wu090@gmail.com>
2018-04-20 09:27:07 -0400

    Modify Human model

--- modo/person/models.py
+++ modo/person/models.py
@@ -1,19 +1,27 @@
+import datetime
+
 from django.db import models
-from django.contrib.auth.base_user import AbstractBaseUser, BaseUserManager
+from django.contrib.auth.base_user import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
 from django.utils.translation import ugettext_lazy as _
 
+from .managers import HumanManager
 
-class Human(AbstractBaseUser):
-    email = models.EmailField(_('Email address'), unique=True)
+
+class Human(AbstractBaseUser, PermissionsMixin):
+    email = models.EmailField(_('Email address'), unique=True, null=False, blank=False,
+                              primary_key=True)
     first_name = models.CharField(_('First name'), max_length=50, blank=True)
     last_name = models.CharField(_('Last name'), max_length=50, blank=True)
-    registered_since = models.DateField(_('Registerd since'), auto_now_add=True)
+    registered_since = models.DateField(_('Registerd since'), default=datetime.date.today())
     is_active = models.BooleanField(_('Active'), default=True)
+    is_staff = models.BooleanField(_('Staff status'), default=False)
     profile_pic = models.ImageField(_('Profile picture'), upload_to='pic/',
                                     null=True, blank=True)
 
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+
+    objects = HumanManager()
 
     def get_full_name(self):
         full_name = '{0} {1}'.format(self.first_name, self.last_name)
