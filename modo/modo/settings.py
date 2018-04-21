aa8269ea49765e2212726c8d0e75f8404a9bce2f
walter_wu090 <walter.wu090@gmail.com>
2018-04-21 15:17:53 -0400

    Update settings

--- modo/modo/settings.py
+++ modo/modo/settings.py
@@ -47,11 +47,12 @@ DJANGO_APPS = [
 ]
 
 THIRD_PARTY_APPS = [
-
+    'rest_framework',
 ]
 
 LOCAL_APPS = [
     'person.apps.PersonConfig',
+    'home.apps.HomeConfig',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
