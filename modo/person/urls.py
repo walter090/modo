3429a0987d149cda1a5589d1ca31d86386207196
walter_wu090 <walter.wu090@gmail.com>
2018-04-21 16:17:29 -0400

    Update routing

--- modo/person/urls.py
+++ modo/person/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
+
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'users', views.HumanView)
 
 urlpatterns = [
-
+    path('', include(router.urls))
 ]
