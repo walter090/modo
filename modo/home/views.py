3e8dddb1325ec0415d189ad231cc05e6f12d9680
walter_wu090 <walter.wu090@gmail.com>
2018-04-21 16:18:47 -0400

    Update view

--- modo/home/views.py
+++ modo/home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+
+class HomeView(View):
+    def get(self, request):
+        return HttpResponse('hi')
