b0911c493824f8ae6aa39109134bc5847c975fef
walter_wu090 <walter.wu090@gmail.com>
2018-04-23 22:43:40 -0400

    Add routing for obtaining new oauth token

--- modo/modo/urls.py
+++ modo/modo/urls.py
@@ -15,10 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', TemplateView.as_view(template_name="index.html")),
     path('account/', include('person.urls')),
+    path('auth/', include('rest_framework_social_oauth2.urls')),
 ]
