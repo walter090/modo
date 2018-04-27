eff36f6bbbda3d8317fdde4ac783f4a39b79a11a
walter090 <walter.wu090@gmail.com>
2018-04-27 17:54:51 -0400

    Update news model
    Update model fields; set all fields except language to be nullable, language field defaults english. Add lookup table Readership.

--- modo/news/models.py
+++ modo/news/models.py
@@ -1,26 +1,33 @@
+import datetime
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
-from modo.person.models import Human
-from modo.modo.util import auxiliary
+from person.models import Human
+from modo.util import auxiliary
 
 
 class Article(models.Model):
     identifier = models.BigIntegerField(_('identifier'), unique=True,
                                         primary_key=True, default=auxiliary.make_id)
     url = models.TextField(_('url'), unique=True)
-    title = models.TextField(_('title'))
-    authors = models.TextField(_('authors'))
-    description = models.TextField(_('description'))
-    language = models.CharField(_('language'), max_length=10)
-    text = models.TextField(_('text'))
-    site_name = models.CharField(_('site name'), max_length=50)
-    tweets = models.TextField(_('tweets'))
-    publish_time = models.DateTimeField(_('publish time'))
-    videos = models.TextField(_('videos'))
-    tags = models.TextField(_('tags'))
+    title = models.TextField(_('title'), null=True)
+    authors = models.TextField(_('authors'), null=True)
+    description = models.TextField(_('description'), null=True)
+    language = models.CharField(_('language'), max_length=10, default='en')
+    text = models.TextField(_('text'), null=True)
+    site_name = models.CharField(_('site name'), max_length=50, null=True)
+    tweets = models.TextField(_('tweets'), null=True)
+    publish_time = models.DateTimeField(_('publish time'), null=True)
+    videos = models.TextField(_('videos'), null=True)
+    tags = models.TextField(_('tags'), null=True)
 
     saved_by = models.ManyToManyField(Human, through='Readership')
 
     def __str__(self):
         return self.title
+
+
+class Readership(models.Model):
+    person = models.ForeignKey(Human, on_delete=models.CASCADE)
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    saved_on = models.DateField(default=datetime.date.today)
