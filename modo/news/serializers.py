aa2c673321457e9e2fd6b658fbfb50a1b947179d
walter090 <walter.wu090@gmail.com>
2018-04-28 21:01:39 -0400

    Add article serializer

---
+++ modo/news/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+
+from .models import Article
+
+
+class ArticleSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Article
+        fields = (
+            'url',
+            'title',
+            'authors',
+            'description',
+            'language',
+            'text',
+            'site_name',
+            'tweets',
+            'publish_time',
+            'videos',
+            'tags'
+        )
