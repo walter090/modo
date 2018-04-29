06bf42c47e5b383200dbd9ddc9930c34415191e4
walter090 <walter.wu090@gmail.com>
2018-04-28 21:00:44 -0400

    Add manager for article

---
+++ modo/news/managers.py
@@ -0,0 +1,43 @@
+from dateutil import parser
+
+from django.db.models import Manager
+
+
+class ArticleManager(Manager):
+    use_in_migration = True
+
+    def create_article(self, url,
+                       title, text,
+                       site_name, publish_time,
+                       authors, description,
+                       tweets, videos,
+                       tags, language='en'):
+        """Function for creating a new article.
+
+        Args:
+            url: str, URL to the article.
+            title: str, Title of the article.
+            text: str, Cleaned text of the article.
+            site_name: str, Name of the source site.
+            publish_time: str, Date time of the article's publishing.
+            authors: list, List of strings of the authors.
+            description: str, Description/subtitle of the article.
+            tweets: list, List of links to tweets in the article.
+            videos: list, List of links to videos in the article.
+            tags: list, List of tags attached to the article.
+            language: str, Code for the language of the article.
+
+        Returns:
+            None.
+        """
+        article = self.model(url=url, title=title,
+                             text=text, site_name=site_name,
+                             language=language, description=description)
+
+        article.publish_time = parser(publish_time)
+        article.authors = ', '.join(authors)
+        article.tweets = ', '.join(tweets)
+        article.videos = ', '.join(videos)
+        article.tags = ', '.join(tags)
+
+        article.save()
