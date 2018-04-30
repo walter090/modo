570254105dc29ad7a6f01ed9634a2c4bde54d3d3
walter090 <walter.wu090@gmail.com>
2018-04-30 00:31:35 -0400

    Move manager, permissions to new dir
    Add constants for person app.

---
+++ modo/person/management/constants.py
@@ -0,0 +1,9 @@
+EDITABLE_FIELDS = ['first_name', 'last_name', 'profile_pic']
+ADMIN_EDITABLE_FIELDS = [
+    'email',
+    'first_name',
+    'last_name',
+    'is_active',
+    'is_staff',
+    'profile_pic'
+]
