12aa960c37ea907f05ac8deb3bd3ad017059eade
walter090 <walter.wu090@gmail.com>
2018-04-30 15:48:02 -0400

    Set custom permission to check if user is staff

--- modo/person/management/permissions.py
+++ modo/person/management/permissions.py
@@ -1,12 +1,12 @@
 from rest_framework.permissions import BasePermission
 
 
-class IsAuthenticatedSelf(BasePermission):
+class IsSelfOrAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
         # Check if the user owns the account
         authenticated_user_id = request.user.identifier
 
-        if obj.identifier == authenticated_user_id:
+        if obj.identifier == authenticated_user_id or request.user.is_staff:
             return True
         else:
             return False
