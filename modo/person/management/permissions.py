0f44efc54652a1a1ac44a9882e2e914dbbdcee83
walter090 <walter.wu090@gmail.com>
2018-04-30 21:24:26 -0400

    Check if user is active in permission

--- modo/person/management/permissions.py
+++ modo/person/management/permissions.py
@@ -5,8 +5,11 @@ class IsSelfOrAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
         # Check if the user owns the account
         authenticated_user_id = request.user.identifier
+        authenticated_user = request.user
 
-        if obj.identifier == authenticated_user_id or request.user.is_staff:
+        if authenticated_user.is_active \
+                and obj.identifier == authenticated_user_id \
+                or request.user.is_staff:
             return True
         else:
             return False
