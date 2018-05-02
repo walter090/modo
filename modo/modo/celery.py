b4a7dc52db2a6ccad5d0c045d412a7779d0d476e
walter090 <walter.wu090@gmail.com>
2018-05-02 15:40:45 -0400

    Update celery

--- modo/modo/celery.py
+++ modo/modo/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'crunchy_waffles.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'modo.settings')
 
 app = Celery('modo')
 
