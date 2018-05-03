2fbd5a23de43ceec546a5f49f31bc59c1565392c
walter090 <walter.wu090@gmail.com>
2018-05-03 17:11:06 -0400

    Update urls

--- modo/modo/urls.py
+++ modo/modo/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('person.urls')),
     path('auth/', include('rest_framework_social_oauth2.urls')),
+    path('news/', include('news.urls')),
 ]
--- modo/news/urls.py
+++ modo/news/urls.py
@@ -4,7 +4,7 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'news', views.NewsView)
+router.register(r'articles', views.NewsView)
 
 urlpatterns = [
     path('', include(router.urls))
