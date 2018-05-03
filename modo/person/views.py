d4dace7af7e287eb143a272e640f6daa9f9aaad5
walter090 <walter.wu090@gmail.com>
2018-05-03 18:25:23 -0400

    Update person view

--- modo/person/views.py
+++ modo/person/views.py
@@ -1,5 +1,4 @@
 from django.http import Http404
-from django.shortcuts import get_object_or_404
 from rest_framework import permissions
 from rest_framework.decorators import action
 from rest_framework.exceptions import PermissionDenied
