0b6c15ff8082de5237d170f7ed8b2abd5ad09aa4
walter090 <walter.wu090@gmail.com>
2018-05-04 17:31:19 -0400

    Add username field to serializer

--- modo/person/serializers.py
+++ modo/person/serializers.py
@@ -6,4 +6,11 @@ from .models import Human
 class HumanSerializer(HyperlinkedModelSerializer):
     class Meta:
         model = Human
-        fields = ('email', 'first_name', 'last_name', 'registered_since')
+        fields = (
+            'username',
+            'email',
+            'first_name',
+            'last_name',
+            'registered_since',
+            'profile_pic'
+        )
