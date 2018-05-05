88a8ad0cd6f31a1d4a394986f8d582372b7aa853
walter090 <walter.wu090@gmail.com>
2018-05-05 02:05:16 -0400

    Make permission check if user is authenticated at all

--- modo/person/management/permissions.py
+++ modo/person/management/permissions.py
@@ -3,6 +3,9 @@ from rest_framework.permissions import BasePermission
 
 class IsSelfOrAdmin(BasePermission):
     def has_object_permission(self, request, view, obj):
+        if request.user.is_anonymous:
+            return False
+
         # Check if the user owns the account
         authenticated_user_id = request.user.identifier
         authenticated_user = request.user
