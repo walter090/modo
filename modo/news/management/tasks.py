798ffc78e1168391f744c90c607c61863986eb00
walter090 <walter.wu090@gmail.com>
2018-05-25 02:12:45 -0400

    Update news sources

--- modo/news/management/tasks.py
+++ modo/news/management/tasks.py
@@ -46,7 +46,7 @@ def update_sources(*args):
     sources = api.get_sources()['sources']
     sources = [source['id'] for source in sources]
     # Exclude Google News
-    regex = re.compile(r'^(?!(google-news|financial-times|fox-sports))')
+    regex = re.compile(r'^(?!(google-news|financial-times|fox-sports|australian-financial-review))')
     sources = list(filter(regex.search, sources))
 
     # Save sources to text file.
