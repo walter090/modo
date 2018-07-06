4a60e78a77abc6acf5d61950f7d5b6f25eeb7235
walter090 <walter.wu090@gmail.com>
2018-07-05 23:42:23 -0400

    Start Post model

--- modo/posts/models.py
+++ modo/posts/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+from modo.util import auxiliary
+
+
+class Post(models.Model):
+    identifier = models.BigIntegerField(_('identifier'), unique=True,
+                                        primary_key=True, default=auxiliary.make_id)
