e2f90ee413f4bbe36cf2f6293edfe9bdeba8f063
walter090 <walter.wu090@gmail.com>
2018-08-07 20:17:44 -0400

    Exclude site from sources

--- modo/news/management/tasks.py
+++ modo/news/management/tasks.py
@@ -70,7 +70,8 @@ def update_sources():
     sources = api.get_sources(language='en')['sources']
     sources = [source['id'] for source in sources]
     # Exclude Google News
-    regex = re.compile(r'^(?!(google-news|financial-times|fox-sports|australian-financial-review|reddit-r-all))')
+    regex = re.compile(r'^(?!(google-news|financial-times|fox-sports|'
+                       r'australian-financial-review|reddit-r-all|the-lad-bible))')
     sources = list(filter(regex.search, sources))
 
     # Save sources to text file.
