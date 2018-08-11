b37ee729c50a3f457f4b8cae6ee6c88f7e87fe2e
walter090 <walter.wu090@gmail.com>
2018-08-10 20:10:57 -0400

    Update Summarizer class
    Add option to fetch information without parsing it. Add dump function to return all results as a python dict. Calculate text shrinkage and return as result.

--- modo/news/management/summary.py
+++ modo/news/management/summary.py
@@ -21,18 +21,28 @@ class Summarizer:
         self.authors = None
         self.canonical_url = None
 
-        self.min_wordcount = 50
-        self.max_wordcount = 150
-        self.result_ratio = 0.2
-        self.num_keywords = 5
+        self.shrinkage = 0
+        self.original_length = -1
+        self.shrunk_length = -1
 
-    def fetch(self, url):
+    def fetch(self, url, num_keywords=5, result_ratio=0.2, min_wordcount=50, max_wordcount=150):
+        self.fetch_only(url)
+        self._parse(num_keywords, result_ratio, min_wordcount, max_wordcount)
+
+    def fetch_only(self, url):
+        """Fetch from source without parsing.
+
+        Args:
+            url: string. URL to the source of article.
+
+        Returns:
+            None
+        """
         goose = Goose()
         article = goose.extract(url)
         self.raw_info = article.infos
-        self._parse()
 
-    def _parse(self):
+    def _parse(self, num_keywords, result_ratio, min_wordcount, max_wordcount):
         self.title = self.raw_info['title']
         self.description = self.raw_info['meta']['description']
         self.text = self.raw_info['cleaned_text']
@@ -40,26 +50,51 @@ class Summarizer:
         self.language = self.raw_info['meta']['lang']
         self.domain = self.raw_info['domain']
         self.type = self.raw_info['opengraph']['type']
-        self.authors = self.raw_info['opengraph']['authors']
+        self.authors = self.raw_info['authors']
         self.canonical_url = self.raw_info['opengraph']['url']
 
-        self.summary = self.summarize_text('. '.join([self.description, self.text]))
-        self.keywords = keywords(text='. '.join([self.title, self.description, self.text]), ratio=0.25,
-                                 words=self.num_keywords, split=True, lemmatize=True)
+        self.summary = self.summarize_text('. '.join([self.description, self.text]),
+                                           result_ratio=result_ratio,
+                                           min_wordcount=min_wordcount,
+                                           max_wordcount=max_wordcount)
+        self.keywords = keywords(text='. '.join([self.title,
+                                                 self.description,
+                                                 self.text]),
+                                 ratio=0.25,
+                                 words=num_keywords, split=True, lemmatize=True)
 
     def dump(self):
+        """ Return all extracted information in a dictionary.
+
+        Returns:
+            extraction: dict. Extracted info in a Python dictionary.
+        """
+        extraction = {key: value for key, value in self.__dict__.items()
+                      if not (key.startswith('raw') or key.startswith('_'))}
+        return extraction
+
+    def dump_raw(self):
+        """ Dump pulled raw information.
+
+        Returns:
+            raw_info: dict. Raw information.
+        """
         return self.raw_info
 
-    def summarize_text(self, text):
-        original_wordcount = self._count_words(text)
-        shrunk_wordcount = int(original_wordcount * self.result_ratio)
+    def summarize_text(self, text, result_ratio=0.2, min_wordcount=50, max_wordcount=150):
+        self.original_length = self._count_words(text)
 
-        if shrunk_wordcount < self.min_wordcount:
-            summary = summarize(text, word_count=self.min_wordcount)
-        elif shrunk_wordcount > self.max_wordcount:
-            summary = summarize(text, word_count=self.max_wordcount)
+        shrunk_wordcount = int(self.original_length * result_ratio)
+
+        if shrunk_wordcount < min_wordcount:
+            summary = summarize(text, word_count=min_wordcount)
+        elif shrunk_wordcount > max_wordcount:
+            summary = summarize(text, word_count=max_wordcount)
         else:
-            summary = summarize(text, ratio=self.result_ratio)
+            summary = summarize(text, ratio=result_ratio)
+
+        self.shrunk_length = self._count_words(summary)
+        self.shrinkage = round((self.original_length - self.shrunk_length) / self.original_length, 2)
 
         return summary
 
